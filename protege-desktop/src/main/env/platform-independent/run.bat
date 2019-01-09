bf007af1919bba7bfaeb026b4eef101096b38ffb
Bob Dionne <dionne@dionne-associates.com>
2019-01-09 11:13:17 -0500

    Update run.bat for new launch class

--- protege-desktop/src/main/env/platform-independent/run.bat
+++ protege-desktop/src/main/env/platform-independent/run.bat
@@ -1,3 +1,3 @@
 setlocal
 cd /d %~dp0
-java -Xmx${conf.mem.xmx}M -Xms${conf.mem.xms}M -Xss${conf.mem.xss}M ${conf.extra.args} -DentityExpansionLimit=100000000 -Dlogback.configurationFile=conf/logback-win.xml -Dfile.encoding=utf-8 -Dorg.protege.plugin.dir=plugins -Dorg.eclipse.equinox.nonosgi.registry.debug=true -classpath "lib/*";"bundles/*";"plugins/*";"bin/*" org.protege.osgi.framework.Launcher %1
+java -Xmx${conf.mem.xmx}M -Xms${conf.mem.xms}M -Xss${conf.mem.xss}M ${conf.extra.args} -DentityExpansionLimit=100000000 -Dlogback.configurationFile=conf/logback-win.xml -Dfile.encoding=utf-8 -Dorg.protege.plugin.dir=plugins -classpath "lib/*";"bundles/*";"plugins/*";"bin/*" org.protege.editor.core.ProtegeApplication %1
