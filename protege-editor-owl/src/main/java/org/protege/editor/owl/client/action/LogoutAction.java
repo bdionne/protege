ea2d84627a3307e26b1d84f125958f92dcb20900
Bob Dionne <dionne@dionne-associates.com>
2024-09-26 16:19:14 +1000

    Prompt user for uncommitted changes on server logout

--- protege-editor-owl/src/main/java/org/protege/editor/owl/client/action/LogoutAction.java
+++ protege-editor-owl/src/main/java/org/protege/editor/owl/client/action/LogoutAction.java
@@ -2,6 +2,8 @@ package org.protege.editor.owl.client.action;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.JOptionPane;
+
 import org.protege.editor.owl.client.api.Client;
 import org.protege.editor.owl.client.event.ClientSessionChangeEvent;
 import org.protege.editor.owl.client.event.ClientSessionListener;
@@ -29,13 +31,32 @@ public class LogoutAction extends AbstractClientAction implements ClientSessionL
     public void dispose() throws Exception {
         super.dispose();
     }
+    
+    public static final String SAVE_CHANGES_MESSAGE = "<html><body><b>You have uncommitted changes. Do you want to save?</b><br>" +
+            "Click 'No' to exit application without saving changes; click 'Yes' to return to the application.</body></html>";
 
     @Override
     public void actionPerformed(ActionEvent event) {
         if (activeClient != null) {
             try {
-                ClientUtils.performLogout(getClientSession(), activeClient);
+            	boolean hasUncommittedChanges = getEditorKit().getModelManager().hasUncommittedChanges();
+        		if (hasUncommittedChanges) {
+        			int ret = JOptionPane.showConfirmDialog(this.getEditorKit().getWorkspace(),
+        					SAVE_CHANGES_MESSAGE,
+        					"Save before exit?",
+        					JOptionPane.YES_NO_OPTION,
+        					JOptionPane.WARNING_MESSAGE);
+        			if (ret == JOptionPane.YES_OPTION || ret == JOptionPane.DEFAULT_OPTION) {
+        				return;
+        			}
+        			if (ret == JOptionPane.NO_OPTION) {
+        				
+        				;
+        			}
+        		} 
+        		ClientUtils.performLogout(getClientSession(), activeClient);
                 setEnabled(false);
+                
             }
             catch (Exception e) {
                 showErrorDialog("Logout error", e.getMessage(), e);
